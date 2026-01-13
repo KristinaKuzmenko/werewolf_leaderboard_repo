a67ea77dc9bbe5f0574188e2a67057b61d8bddcf
Krystyna Kuzmenko <kristina_neskey@icloud.com>
2026-01-13 17:31:09 +0200

    fix compose configs

--- generate_compose.py
+++ generate_compose.py
@@ -62,7 +62,7 @@ services:
     image: {green_image}
     platform: linux/amd64
     container_name: green-agent
-    command: ["--host", "0.0.0.0", "--port", "{green_port}"]
+    command: ["python", "src/server.py", "--host", "0.0.0.0", "--port", "{green_port}"]
     env_file:
       - .env
     environment:{green_env}
